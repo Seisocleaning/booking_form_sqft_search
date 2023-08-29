ac8d4498af3134ff75e9eb6fefd838eef7b9e1c6
Seisocleaning <143470145+Seisocleaning@users.noreply.github.com>
2023-08-28 20:23:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,50 @@
-// Initialize Google Places Autocomplete
-const addressInput = document.getElementById('addressInput');
-const autocomplete = new google.maps.places.Autocomplete(addressInput);
-
 const searchButton = document.getElementById('searchButton');
+const addressInput = document.getElementById('addressInput');
 const resultContainer = document.getElementById('resultContainer');
 
 let lastRequestTime = 0; // Store the timestamp of the last request
 
-autocomplete.addListener('place_changed', () => {
-  const selectedPlace = autocomplete.getPlace();
-  if (selectedPlace && selectedPlace.formatted_address) {
-    addressInput.value = selectedPlace.formatted_address;
-    performSearch(selectedPlace.formatted_address);
-  }
-});
-
-searchButton.addEventListener('click', () => {
+searchButton.addEventListener('click', async () => {
   const address = addressInput.value;
+
   if (address.trim() !== '') {
-    performSearch(address);
+    const currentTime = new Date().getTime();
+    const timeSinceLastRequest = currentTime - lastRequestTime;
+
+    if (timeSinceLastRequest < 500) { // Ensure at least 500 milliseconds between requests (2 requests per second)
+      resultContainer.innerHTML = 'Please wait a moment before making another request.';
+      return;
+    }
+
+    try {
+      lastRequestTime = currentTime; // Update the last request time
+      const response = await fetch(`https://zillow-working-api.p.rapidapi.com/search/byaddress?query=${encodeURIComponent(address)}`, {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'b992a0a4c9mshe98194ed211e071p1a8a00jsn9be933e403ac',
+          'X-RapidAPI-Host': 'zillow-working-api.p.rapidapi.com'
+        }
+      });
+
+      const data = await response.json();
+      if (data && data.Results && data.Results.length > 0) {
+        displayResult(data.Results[0]);
+      } else {
+        resultContainer.innerHTML = 'No property information available.';
+      }
+    } catch (error) {
+      console.error(error);
+      resultContainer.innerHTML = 'Error fetching data: ' + error.message;
+    }
   }
 });
 
-function performSearch(address) {
-  const currentTime = new Date().getTime();
-  const timeSinceLastRequest = currentTime - lastRequestTime;
-
-  if (timeSinceLastRequest < 500) {
-    resultContainer.innerHTML = 'Please wait a moment before making another request.';
-    return;
-  }
-
-  lastRequestTime = currentTime;
-  fetchPropertyInformation(address);
-}
-
-async function fetchPropertyInformation(address) {
-  try {
-    const response = await fetch(`https://zillow-working-api.p.rapidapi.com/search/byaddress?query=${encodeURIComponent(address)}`, {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'b992a0a4c9mshe98194ed211e071p1a8a00jsn9be933e403ac',
-        'X-RapidAPI-Host': 'zillow-working-api.p.rapidapi.com'
-      }
-    });
-
-    const data = await response.json();
-    displayResult(data);
-  } catch (error) {
-    console.error(error);
-    resultContainer.innerHTML = 'Error fetching data: ' + error.message;
-  }
-}
-
-function displayResult(data) {
-  console.log(data); // Log the response data to the console for inspection
-
-  // Assuming the address and area data are in the correct format
-  const result = data.Results[0];
+function displayResult(result) {
+  console.log(result); // Log the response data to the console for inspection
 
   // Display the area value only
   resultContainer.innerHTML = `
     <h2>Property Information</h2>
-    <p>Area: ${result.area} sqft</p>
+    <p>Area: ${result.area} sq ft</p>
   `;
 }
