69aecc96c2d6cbc0fc35dbed6742eef1ed771f65
Seisocleaning <143470145+Seisocleaning@users.noreply.github.com>
2023-08-28 22:32:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const searchButton = document.getElementById('searchButton');
-  const clearButton = document.getElementById('clearButton');
   const propertyAddressInput = document.getElementById('propertyAddressInput');
-  const unitNumberContainer = document.getElementById('unitNumberContainer');
-  const unitNumberInput = document.getElementById('unitNumberInput');
   const resultContainer = document.getElementById('resultContainer');
 
   searchButton.addEventListener('click', async () => {
     const propertyAddress = propertyAddressInput.value;
     if (propertyAddress.trim() !== '') {
-      if (propertyAddress.includes('#')) {
-        unitNumberContainer.style.display = 'block';
-        return;
-      }
       try {
         const url = `https://zillow-working-api.p.rapidapi.com/client/byaddress?propertyaddress=${encodeURIComponent(propertyAddress)}`;
         const options = {
@@ -29,37 +22,10 @@ document.addEventListener('DOMContentLoaded', () => {
         displayResult(data);
       } catch (error) {
         console.error(error);
-        resultContainer.innerHTML = 'Error fetching data. Please try again later.';
+        resultContainer.innerHTML = 'Error fetching data: ' + error.message;
       }
     }
   });
-
-  clearButton.addEventListener('click', () => {
-    propertyAddressInput.value = '';
-    unitNumberInput.value = '';
-    unitNumberContainer.style.display = 'none';
-    resultContainer.innerHTML = '';
-  });
-
-  unitNumberInput.addEventListener('keyup', (event) => {
-    if (event.key === 'Enter') {
-      searchWithUnitNumber();
-    }
-  });
-
-  function searchWithUnitNumber() {
-    const propertyAddress = propertyAddressInput.value;
-    const unitNumber = unitNumberInput.value;
-    
-    if (propertyAddress.trim() !== '' && unitNumber.trim() !== '') {
-      const fullPropertyAddress = `${propertyAddress} ${unitNumber}`;
-      unitNumberContainer.style.display = 'none';
-      
-      // ... (existing code for API request with fullPropertyAddress)
-      
-      displayResult(data);
-    }
-  }
 
   function displayResult(data) {
     if (data && data.propertyDetails && data.propertyDetails.livingArea) {
