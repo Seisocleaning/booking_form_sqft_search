18e17ea09fd32cc476b4f0bd07fe56a498723480
Seisocleaning <143470145+Seisocleaning@users.noreply.github.com>
2023-08-28 22:06:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,24 +4,28 @@ document.addEventListener('DOMContentLoaded', () => {
   const propertyAddressInput = document.getElementById('propertyAddressInput');
   const unitNumberContainer = document.getElementById('unitNumberContainer');
   const unitNumberInput = document.getElementById('unitNumberInput');
+  const unitNumberSubmit = document.getElementById('unitNumberSubmit');
   const resultContainer = document.getElementById('resultContainer');
 
-  searchButton.addEventListener('click', async () => {
-    const propertyAddress = propertyAddressInput.value;
-    if (propertyAddress.trim() !== '') {
+  searchButton.addEventListener('click', () => {
+    const propertyAddress = propertyAddressInput.value.trim();
+    if (propertyAddress !== '') {
       if (propertyAddress.includes('#')) {
         unitNumberContainer.style.display = 'block';
         return;
       }
 
-      try {
-        // ... (existing code for API request)
+      fetchPropertyData(propertyAddress);
+    }
+  });
 
-        displayResult(data);
-      } catch (error) {
-        console.error(error);
-        resultContainer.innerHTML = 'Error fetching data. Please try again later.';
-      }
+  unitNumberSubmit.addEventListener('click', () => {
+    const propertyAddress = propertyAddressInput.value.trim();
+    const unitNumber = unitNumberInput.value.trim();
+    if (propertyAddress !== '' && unitNumber !== '') {
+      unitNumberContainer.style.display = 'none';
+      const fullPropertyAddress = `${propertyAddress} ${unitNumber}`;
+      fetchPropertyData(fullPropertyAddress);
     }
   });
 
@@ -32,27 +36,28 @@ document.addEventListener('DOMContentLoaded', () => {
     resultContainer.innerHTML = '';
   });
 
-  unitNumberInput.addEventListener('keyup', (event) => {
-    if (event.key === 'Enter') {
-      searchWithUnitNumber();
-    }
-  });
+  async function fetchPropertyData(address) {
+    try {
+      const url = `https://zillow-working-api.p.rapidapi.com/client/byaddress?propertyaddress=${encodeURIComponent(address)}`;
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'b992a0a4c9mshe98194ed211e071p1a8a00jsn9be933e403ac',
+          'X-RapidAPI-Host': 'zillow-working-api.p.rapidapi.com'
+        }
+      };
 
-  function searchWithUnitNumber() {
-    const propertyAddress = propertyAddressInput.value;
-    const unitNumber = unitNumberInput.value;
-    
-    if (propertyAddress.trim() !== '' && unitNumber.trim() !== '') {
-      const fullPropertyAddress = `${propertyAddress} ${unitNumber}`;
-      unitNumberContainer.style.display = 'none';
-      
-      // ... (existing code for API request with fullPropertyAddress)
-      
+      const response = await fetch(url, options);
+      const data = await response.json();
+
       displayResult(data);
+    } catch (error) {
+      console.error(error);
+      resultContainer.innerHTML = 'Error fetching data. Please try again later.';
     }
   }
 
   function displayResult(data) {
-    // ... (existing code to display property details)
+    // Display property details in the resultContainer
   }
 });
