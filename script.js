fbf65e19b9e1764d0a761a47f76e702b41659582
Seisocleaning <143470145+Seisocleaning@users.noreply.github.com>
2023-08-28 21:16:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
   const searchButton = document.getElementById('searchButton');
-  const propertyAddressInput = document.getElementById('propertyaddress');
-  const resultContainer = document.getElementById('propertyaddress');
+  const propertyAddressInput = document.getElementById('propertyAddressInput');
+  const resultContainer = document.getElementById('resultContainer');
 
   searchButton.addEventListener('click', async () => {
-    const propertyAddress = propertyaddress.value;
+    const propertyAddress = propertyAddressInput.value;
     if (propertyAddress.trim() !== '') {
-      try {
-        const url = `https://zillow-working-api.p.rapidapi.com/client/byaddress?propertyaddress=${encodeURIComponent(propertyAddress)}`;
-        const options = {
-          method: 'GET',
-          headers: {
-            'X-RapidAPI-Key': 'b992a0a4c9mshe98194ed211e071p1a8a00jsn9be933e403ac',
-            'X-RapidAPI-Host': 'zillow-working-api.p.rapidapi.com'
-          }
-        };
+      const url = `https://zillow-working-api.p.rapidapi.com/client/byaddress?propertyaddress=${encodeURIComponent(propertyAddress)}`;
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'b992a0a4c9mshe98194ed211e071p1a8a00jsn9be933e403ac',
+          'X-RapidAPI-Host': 'zillow-working-api.p.rapidapi.com'
+        }
+      };
 
+      try {
         const response = await fetch(url, options);
         const result = await response.text();
-
-        displayResult(result);
+        console.log(result);
+        resultContainer.innerHTML = result; // Display the result
       } catch (error) {
         console.error(error);
         resultContainer.innerHTML = 'Error fetching data: ' + error.message;
       }
     }
   });
-
-  function displayResult(result) {
-    // Your display result logic here
-    resultContainer.innerHTML = result;
-  }
 });
