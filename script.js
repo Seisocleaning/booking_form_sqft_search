debefc2619e1f2bd5251443458c29acc46f6c61f
Seisocleaning <143470145+Seisocleaning@users.noreply.github.com>
2023-08-28 21:02:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
-// Make sure to include your import statements here
 import axios from 'axios';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const addressInput = document.getElementById('addressInput');
   const searchButton = document.getElementById('searchButton');
+  const addressInput = document.getElementById('addressInput');
   const resultContainer = document.getElementById('resultContainer');
 
+  // Initialize Google Places Autocomplete
+  const autocomplete = new google.maps.places.Autocomplete(addressInput);
+
   searchButton.addEventListener('click', async () => {
-    const address = addressInput.value.trim();
-    if (address === '') {
-      return;
-    }
+    const userInput = addressInput.value;
+    const formattedAddress = formatAddressInput(userInput);
 
-    const url = `https://zillow-working-api.p.rapidapi.com/client/byaddress?propertyaddress=${encodeURIComponent(address)}`;
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'b992a0a4c9mshe98194ed211e071p1a8a00jsn9be933e403ac',
-        'X-RapidAPI-Host': 'zillow-working-api.p.rapidapi.com'
+    if (formattedAddress.trim() !== '') {
+      try {
+        const response = await axios.get('https://zillow-working-api.p.rapidapi.com/client/byaddress', {
+          params: {
+            propertyaddress: formattedAddress
+          },
+          headers: {
+            'X-RapidAPI-Key': 'b992a0a4c9mshe98194ed211e071p1a8a00jsn9be933e403ac',
+            'X-RapidAPI-Host': 'zillow-working-api.p.rapidapi.com'
+          }
+        });
+
+        const data = response.data;
+        displayResult(data);
+      } catch (error) {
+        console.error(error);
+        resultContainer.innerHTML = 'Error fetching data: ' + error.message;
       }
-    };
-
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json(); // Parse the JSON response
-      displayResult(result); // Call the function to display the result
-    } catch (error) {
-      console.error(error);
     }
   });
 
-  function displayResult(result) {
-    // Your code to display the result goes here
-    // You can access properties of the result object and update the resultContainer
+  function displayResult(data) {
+    if (data.Results && data.Results.length > 0) {
+      const result = data.Results[0];
+
+      resultContainer.innerHTML = `
+        <h2>Property Information</h2>
+        <p>Address: ${result.address}</p>
+        <p>Area: ${result.area} sq ft</p>
+      `;
+    } else {
+      resultContainer.innerHTML = 'No property found.';
+    }
+  }
+
+  function formatAddressInput(input) {
+    // You can customize this function to format the user input as needed
+    return input;
   }
 });
