2315a8c50232b31841b2c5b778c43501dad5370f
Seisocleaning <143470145+Seisocleaning@users.noreply.github.com>
2023-08-28 21:01:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,37 @@
+// Make sure to include your import statements here
+import axios from 'axios';
+
 document.addEventListener('DOMContentLoaded', () => {
-  const searchButton = document.getElementById('searchButton');
   const addressInput = document.getElementById('addressInput');
+  const searchButton = document.getElementById('searchButton');
   const resultContainer = document.getElementById('resultContainer');
 
-  searchButton.addEventListener('click', fetchPropertyInfo);
+  searchButton.addEventListener('click', async () => {
+    const address = addressInput.value.trim();
+    if (address === '') {
+      return;
+    }
 
-  async function fetchPropertyInfo() {
-    const userInput = addressInput.value;
-    const formattedAddress = formatAddressInput(userInput);
-
-    if (formattedAddress.trim() !== '') {
-      const url = `https://zillow-working-api.p.rapidapi.com/client/byaddress?propertyaddress=${encodeURIComponent(formattedAddress)}`;
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': 'b992a0a4c9mshe98194ed211e071p1a8a00jsn9be933e403ac',
-          'X-RapidAPI-Host': 'zillow-working-api.p.rapidapi.com'
-        }
-      };
-
-      try {
-        const response = await fetch(url, options);
-        const data = await response.json();
-        displayResult(data);
-      } catch (error) {
-        console.error(error);
-        resultContainer.innerHTML = 'Error fetching data: ' + error.message;
+    const url = `https://zillow-working-api.p.rapidapi.com/client/byaddress?propertyaddress=${encodeURIComponent(address)}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'b992a0a4c9mshe98194ed211e071p1a8a00jsn9be933e403ac',
+        'X-RapidAPI-Host': 'zillow-working-api.p.rapidapi.com'
       }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json(); // Parse the JSON response
+      displayResult(result); // Call the function to display the result
+    } catch (error) {
+      console.error(error);
     }
-  }
+  });
 
-  function displayResult(data) {
-    if (data.propertyDetails) {
-      const property = data.propertyDetails;
-
-      resultContainer.innerHTML = `
-        <h2>Property Information</h2>
-        <p>Address: ${property.address.streetAddress}, ${property.address.city}, ${property.address.state} ${property.address.zipcode}</p>
-        <p>Area: ${property.livingArea || property.livingAreaValue} sq ft</p>
-      `;
-    } else {
-      resultContainer.innerHTML = 'No property found.';
-    }
-  }
-
-  function formatAddressInput(input) {
-    return input;
+  function displayResult(result) {
+    // Your code to display the result goes here
+    // You can access properties of the result object and update the resultContainer
   }
 });
